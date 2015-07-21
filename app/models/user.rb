626f514720cc3f9fc8303de38404cff9312357b8
Durkheim <mosesyk.dev.bootcamp@gmail.com>
2015-07-21 18:15:30 -0500

    reviewing associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :photos
   has_many :comments, foreign_key: :commenter_id
-  #don't know if we actually need this association but I built it out anyway 
+  #don't know if we actually need this association but I built it out anyway
   has_many :commented_photos, through: :comments, source: :photo
   has_many :visited_countries
   has_many :countries, through: :visited_countries
