b8fb90d5278232c9410408886f84e207f93b57de
BackStreetSoy <jordanedwardobey@gmail.com>
2015-07-24 16:37:46 -0500

    added signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      redirect_to user_path(@user)
+    else 
+      redirect_to root_url
+    end
   end
 
   def update
@@ -17,4 +24,10 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :avatar, :bio, :private)
+  end 
 end
