168af1abbd0c852e65e305f5745ddcd6a1fe2987
BackStreetSoy <jordanedwardobey@gmail.com>
2015-07-24 16:08:55 -0500

    added login, authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_secure_password 
+  
   has_many :photos
   has_many :comments, foreign_key: :commenter_id
   has_many :commented_photos, through: :comments, source: :photo
@@ -8,13 +10,9 @@ class User < ActiveRecord::Base
   has_many :friendships
   has_many :friends, through: :friendships
 
-<<<<<<< HEAD
-  validates :email, :format => /.+@.+\..+/
-  validates :username, :email, :presence => true, :uniqueness => true 
-=======
   validates :email, :presence=> true, :uniqueness => true, :format => /.+@.+\..+/
   validates :username, :presence => true, :uniqueness => true 
->>>>>>> 5d4c8fe3e77af834b452fd332a0037271ad8ed6d
+
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -81,12 +81,12 @@ ActiveRecord::Schema.define(version: 20150714035217) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "bio"
     t.string   "avatar"
     t.boolean  "private"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "visited_countries", force: :cascade do |t|
