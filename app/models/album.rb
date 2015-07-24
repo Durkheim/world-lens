723e55b80289c4b95ab61814caa6c9e78d2f2db2
BackStreetSoy <jordanedwardobey@gmail.com>
2015-07-23 19:15:56 -0500

    added user access to album model

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,10 @@
 class Album < ActiveRecord::Base
   belongs_to :visited_country
   has_many :photos
+
+  validates :name, :presence => true 
+
+  def user 
+    self.visited_country.user
+  end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ActiveRecord::Base
   has_many :photos
   has_many :comments, foreign_key: :commenter_id
-  #don't know if we actually need this association but I built it out anyway
   has_many :commented_photos, through: :comments, source: :photo
   has_many :visited_countries
   has_many :countries, through: :visited_countries
   has_many :albums, through: :visited_countries
   has_many :friendships
   has_many :friends, through: :friendships
+
+  validates :email, :format => /.+@.+\..+/
+  validates :username, :email, :presence => true, :uniqueness => true 
+
+
 end
