8ea577dcfadaafe41ecbff1bb08319cdc43ecd26
Durkheim <mosesyk.dev.bootcamp@gmail.com>
2015-07-24 14:15:18 -0500

    generating welcome and user routes/controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
+
+  get 'user/index'
+
+  get 'user/show'
+
+  get 'user/new'
+
+  get 'user/create'
+
+  get 'user/update'
+
+  get 'user/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
