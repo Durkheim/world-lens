d928d4d91b23a960aec5609edaa560304806fe5e
Durkheim <mosesyk.dev.bootcamp@gmail.com>
2015-10-01 19:40:52 -0500

    updated friendship association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  has_secure_password 
-  
+  has_secure_password
+
   has_many :photos
   has_many :comments, foreign_key: :commenter_id
   has_many :commented_photos, through: :comments, source: :photo
@@ -9,9 +9,11 @@ class User < ActiveRecord::Base
   has_many :albums, through: :visited_countries
   has_many :friendships
   has_many :friends, through: :friendships
+  has_many :friendships_as_friend, foreign_key: :friend_id, class_name: "Friendship"
+  has_many :friends_as_friend, through: :friendships_as_friend, source: :user
 
   validates :email, :presence=> true, :uniqueness => true, :format => /.+@.+\..+/
-  validates :username, :presence => true, :uniqueness => true 
+  validates :username, :presence => true, :uniqueness => true
 
 
 
